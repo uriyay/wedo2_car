d1a6f8d1321e42e49a38d7a9742cd3543cbff526
uriyay <uriyay@gmail.com>
2023-10-22 18:03:11 +0300

    fixed cnc.py, added cnc_client.py for running from a PC

--- cnc.py
+++ cnc.py
@@ -2,27 +2,30 @@ import json
 import network
 import socket
 import struct
-from time import sleep
+from time import sleep, ticks_ms
 
-from wedo2 import wedo2
+import wedo2
 
 wlan = network.WLAN(network.STA_IF)
-config = json.load(open('config.json'))
+config = json.load(open("config.json"))
 
 PORT = 7777
+TIMEOUT_MS = 300
+
 
 def init_network():
-    #Notice that Windows blocks incoming connections..
+    # Notice that Windows blocks incoming connections..
     wlan.active(True)
     if not wlan.isconnected():
-        print('connecting to network...')
-        wlan.connect(config['SSID'], config['PASSWORD'])
+        print("connecting to network...")
+        wlan.connect(config["SSID"], config["PASSWORD"])
         while not wlan.isconnected():
             pass
 
     wlan_config = wlan.ifconfig()
-    print('network config:', wlan_config)
+    print("network config:", wlan_config)
     return wlan_config
+
 
 class CNC:
     def __init__(self, port=PORT):
@@ -33,96 +36,173 @@ class CNC:
         self.wedo = None
 
     def advertise(self):
-        sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM, socket.IPPROTO_UDP)  # UDP
+        sock = socket.socket(
+            socket.AF_INET, socket.SOCK_DGRAM, socket.IPPROTO_UDP
+        )  # UDP
         sock.bind((self.ip, self.port))
-        msg = ('Hello from CNC, listening on UDP socket ip=' + self.ip + ', port=' + str(self.port)).encode()
+        msg = (
+            "Hello from CNC, listening on UDP socket ip="
+            + self.ip
+            + ", port="
+            + str(self.port)
+        ).encode()
         sock.sendto(msg, ("255.255.255.255", self.port))
         sock.close()
 
     def run(self):
         self.advertise()
-        sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM, socket.IPPROTO_UDP)  # UDP
+        sock = socket.socket(
+            socket.AF_INET, socket.SOCK_DGRAM, socket.IPPROTO_UDP
+        )  # UDP
         sock.bind((self.ip, self.port))
-        sock.listen(1)
         self.is_running = True
         while self.is_running:
-            client, clientaddr = sock.accept()
-            self.handle_client(self, client)
+            data, req_addrinfo = sock.recvfrom(0x10)
+            print('data = "{}", req_addrinfo = {}'.format(data, req_addrinfo))
+            if data == b"HELLO":
+                print("handling client..")
+                self.handle_client(sock, req_addrinfo)
         sock.close()
 
-    def response(self, client, res):
+    def response(self, client, addrinfo, res):
         data = json.dumps(res).encode()
-        client.write(struct.pack('<H', len(data)) + data)
+        client.sendto(struct.pack("<H", len(data)) + data, addrinfo)
 
-    def handle_client(self, client):
+    def read_from_client(self, client, addrinfo, size):
+        start_time = ticks_ms()
+        cur = start_time
+        while cur - start_time <= TIMEOUT_MS:
+            data, req_addrinfo = client.recvfrom(size)
+            print('got data: {}'.format(data))
+            if req_addrinfo == addrinfo:
+                return data
+            cur = tick_ms()
+        raise Exception("Socket timeout!")
+
+    def handle_client(self, client, addrinfo):
         is_client_running = True
         while is_client_running:
-            datalen = struct.unpack('<H', client.read(2))
-            data = client.read(datalen)
+            datalen = struct.unpack("<H", self.read_from_client(client, addrinfo, 2))[0]
+            print("got packet with datalen = {}".format(datalen))
+            data = self.read_from_client(client, addrinfo, datalen)
             cmd = json.loads(data)
-            if cmd['cmd'] == 'quit':
-                self.response(client, {'res': '0', 'msg': 'bye!'})
+            print("got cmd: {}".format(str(cmd)))
+            if cmd["cmd"] == "quit":
+                self.response(client, addrinfo, {"res": "0", "msg": "bye!"})
                 self.is_running = False
                 is_client_running = False
-            elif cmd['cmd'] == 'connect':
+            elif cmd["cmd"] == "echo":
+                self.response(client, addrinfo, {"res": "0", "msg": "echo response"})
+            elif cmd["cmd"] == "connect":
                 try:
                     self.wedo = wedo2.Wedo2()
                     self.wedo.scan()
-                    self.response(client, {'res': '0', 'msg': 'wedo2 connected!'})
+                    self.response(
+                        client, addrinfo, {"res": "0", "msg": "wedo2 connected!"}
+                    )
                 except Exception as e:
-                    self.response(client, {'res': '1', 'msg': 'Failed to connect wedo2, error: ' + str(e)})
+                    self.response(
+                        client,
+                        addrinfo,
+                        {
+                            "res": "1",
+                            "msg": "Failed to connect wedo2, error: " + str(e),
+                        },
+                    )
                     self.wedo = None
-            elif cmd['cmd'] == 'disconnect':
+            elif cmd["cmd"] == "disconnect":
                 if self.wedo:
                     try:
                         self.wedo.disconnect()
-                        self.response(client, {'res': '0', 'msg': 'wedo2 disconnected!'})
+                        self.response(
+                            client, addrinfo, {"res": "0", "msg": "wedo2 disconnected!"}
+                        )
                     except Exception as e:
-                        self.response(client, {'res': '1', 'msg': 'Failed to disconnect wedo2, error: ' + str(e)})
+                        self.response(
+                            client,
+                            addrinfo,
+                            {
+                                "res": "1",
+                                "msg": "Failed to disconnect wedo2, error: " + str(e),
+                            },
+                        )
                     self.wedo = None
-            elif cmd['cmd'] == 'up':
+            elif cmd["cmd"] == "up":
                 if not self.wedo:
-                    self.response(client, {'res': '1', 'msg': 'wedo2 is not connected!'})
+                    self.response(
+                        client, addrinfo, {"res": "1", "msg": "wedo2 is not connected!"}
+                    )
                 else:
                     try:
-                        #TODO: parameters can be passed in the json later
+                        # TODO: parameters can be passed in the json later
                         self.wedo.motor_turn(0, 100)
                         sleep(1)
                         self.wedo.motor_break(0)
-                        self.response(client, {'res': '0', 'msg': 'up succeeded'})
+                        self.response(
+                            client, addrinfo, {"res": "0", "msg": "up succeeded"}
+                        )
                     except Exception as e:
-                        self.response(client, {'res': '1', 'msg': 'up failed: ' + str(e)})
-            elif cmd['cmd'] == 'down':
+                        self.response(
+                            client,
+                            addrinfo,
+                            {"res": "1", "msg": "up failed: " + str(e)},
+                        )
+            elif cmd["cmd"] == "down":
                 if not self.wedo:
-                    self.response(client, {'res': '1', 'msg': 'wedo2 is not connected!'})
+                    self.response(
+                        client, addrinfo, {"res": "1", "msg": "wedo2 is not connected!"}
+                    )
                 else:
                     try:
                         self.wedo.motor_turn(0, -100)
                         sleep(1)
                         self.wedo.motor_break(0)
-                        self.response(client, {'res': '0', 'msg': 'down succeeded'})
+                        self.response(
+                            client, addrinfo, {"res": "0", "msg": "down succeeded"}
+                        )
                     except Exception as e:
-                        self.response(client, {'res': '1', 'msg': 'down failed: ' + str(e)})
-            elif cmd['cmd'] == 'right':
+                        self.response(
+                            client,
+                            addrinfo,
+                            {"res": "1", "msg": "down failed: " + str(e)},
+                        )
+            elif cmd["cmd"] == "right":
                 if not self.wedo:
-                    self.response(client, {'res': '1', 'msg': 'wedo2 is not connected!'})
+                    self.response(
+                        client, addrinfo, {"res": "1", "msg": "wedo2 is not connected!"}
+                    )
                 else:
                     try:
                         self.wedo.motor_turn(1, 10)
                         sleep(2)
                         self.wedo.motor_break(1)
-                        self.response(client, {'res': '0', 'msg': 'right succeeded'})
+                        self.response(
+                            client, addrinfo, {"res": "0", "msg": "right succeeded"}
+                        )
                     except Exception as e:
-                        self.response(client, {'res': '1', 'msg': 'right failed: ' + str(e)})
-            elif cmd['cmd'] == 'left':
+                        self.response(
+                            client,
+                            addrinfo,
+                            {"res": "1", "msg": "right failed: " + str(e)},
+                        )
+            elif cmd["cmd"] == "left":
                 if not self.wedo:
-                    self.response(client, {'res': '1', 'msg': 'wedo2 is not connected!'})
+                    self.response(
+                        client, addrinfo, {"res": "1", "msg": "wedo2 is not connected!"}
+                    )
                 else:
                     try:
                         self.wedo.motor_turn(1, -10)
                         sleep(2)
                         self.wedo.motor_break(1)
-                        self.response(client, {'res': '0', 'msg': 'left succeeded'})
+                        self.response(
+                            client, addrinfo, {"res": "0", "msg": "left succeeded"}
+                        )
                     except Exception as e:
-                        self.response(client, {'res': '1', 'msg': 'left failed: ' + str(e)})
+                        self.response(
+                            client, {"res": "1", "msg": "left failed: " + str(e)}
+                        )
 
+
+c = CNC()
+c.run()
---
+++ cnc_client.py
@@ -0,0 +1,82 @@
+import socket
+import struct
+import json
+import re
+from scapy.all import sniff, UDP
+from time import sleep
+
+PORT = 7777
+
+class CNCClient:
+    def __init__(self, ip=None, port=None):
+        self.ip = None
+        self.port = None
+        if ip:
+            self.ip = ip
+        if port:
+            self.port = port
+        if (not self.ip) or (not self.port):
+            #get the connection details automatically
+            self.ip, self.port = self.get_connection_details()
+
+        print('connecting to ip={}, port={}'.format(self.ip, self.port))
+        self.sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM, socket.IPPROTO_UDP)  # UDP
+
+    def get_connection_details(self):
+        pkts = sniff(count=1, filter="udp and port 7777")
+        data = bytes(pkts[0][UDP].payload)
+        data = data.decode()
+        m = re.search('.*ip=(.+), port=(\d+)', data)
+        ip, port = m.groups()
+        port = int(port)
+        return ip, port
+    
+    def connect(self):
+        self.sock.sendto(b'HELLO', (self.ip, self.port))
+
+    def send_command(self, json_obj):
+        data = json.dumps(json_obj).encode()
+        print('sending command: {}'.format(data))
+        self.sock.sendto(struct.pack('<H', len(data)), (self.ip, self.port))
+        self.sock.sendto(data, (self.ip, self.port))
+        recv_data, _ = self.sock.recvfrom(1024)
+        resp_len = struct.unpack('<H', recv_data[:2])[0]
+        resp = recv_data[2:2 + resp_len]
+        resp_json = json.loads(resp)
+        print('response: ' + str(resp_json))
+        return resp_json
+
+    def quit(self):
+        self.send_command({'cmd': 'quit'})
+
+    def echo(self):
+        self.send_command({'cmd': 'echo'})
+
+    def wedo2_connect(self):
+        self.send_command({'cmd': 'connect'})
+
+    def wedo2_disconnect(self):
+        self.send_command({'cmd': 'disconnect'})
+
+    def up(self):
+        self.send_command({'cmd': 'up'})
+
+    def down(self):
+        self.send_command({'cmd': 'down'})
+
+    def right(self):
+        self.send_command({'cmd': 'right'})
+
+    def left(self):
+        self.send_command({'cmd': 'left'})
+
+
+if __name__ == '__main__':
+    c = CNCClient()
+    c.connect()
+    c.echo()
+    c.wedo2_connect()
+    c.up()
+    sleep(3)
+    c.wedo2_disconnect()
+    c.quit()
